b2d30295d3d52b09c7695ab7632d8170be527835
DevSouza05 <peterson.moraes2005@gmail.com>
2024-02-19 16:16:32 -0300

    update i  client server

--- Servidor/servidor.py
+++ Servidor/servidor.py
@@ -3,13 +3,18 @@ import socket
 # criando o objeto
 servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-servidor.bind(('localhost','7777'))
+servidor.bind(('localhost',7777))
 servidor.listen(1)
 
+#criando objeto de conexao com cliente
 connection, address =servidor.accept()
 
 namefile= connection.recv(1024).decode()
 
+#rb:read byte
 with open(namefile,'rb') as file:
     for data in file .readlines():
         connection.send(data)
+
+
+        print("ARQUIVO ENVIADO!")
